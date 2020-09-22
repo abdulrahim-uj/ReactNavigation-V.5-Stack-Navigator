7c25baed96d7774892f79f5e24a55ee17ea24265
AbdulRahim <abdulrahim.uj@gmail.com>
2020-09-22 09:34:05 +0530

    Final Commit on Stack Navigation Commit: v.2

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { View, Text } from 'react-native-animatable';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import Home_Screen from './src/components/screeners/HomeScreen';
+import Detail_Screen from './src/components/screeners/DetailScreen';
 
 
 
@@ -11,183 +10,7 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 
 
 
-const HomeScreen = ({navigation}) => {
-    return(
-        <SafeAreaView 
-            style={
-                {
-                    flex: 1, 
-                    alignItems: "center", 
-                    justifyContent: "center"}}>
-            <View 
-                style={
-                    {
-                        flex: 1, 
-                        alignItems: "center", 
-                        justifyContent: "center"}}>
-                <Text>Home Screen</Text>
-                <TouchableOpacity 
-                    onPress={
-                        () => navigation.navigate("Details")
-                    }
-                    style={
-                        {
-                            backgroundColor: "#FF2D55",
-                            paddingHorizontal: 30,
-                            paddingVertical: 7,
-                            textAlign: "center",
-                            borderRadius: 3,
-                        }
-                    }
-                    >
-                    <Text style={
-                        {
-                            color: "#fff",
-                            fontSize: 15
-                        }
-                    }>Go To Details Screen
-                    </Text>
-                </TouchableOpacity>
-            </View>
-        </SafeAreaView>
-    );
-};
 
-const DetailScreen = ({navigation}) => {
-    return(
-        <SafeAreaView 
-            style={
-                {
-                    flex: 1, 
-                    alignItems: "center", 
-                    justifyContent: "center"}}>
-            <View 
-                style={
-                    {
-                        flex: 1, 
-                        alignItems: "center", 
-                        justifyContent: "center"}}>
-                <Text>Detail Screen</Text>
-
-                <View style={
-                    {
-                        paddingBottom: 10,
-                    }
-                }>
-                    <TouchableOpacity 
-                        onPress={
-                            () => navigation.push("Details")
-                        }
-                        style={
-                            {
-                                backgroundColor: "#FF2D55",
-                                paddingHorizontal: 30,
-                                paddingVertical: 7,
-                                textAlign: "center",
-                                borderRadius: 3,
-                            }
-                        }
-                        >
-                        <Text style={
-                            {
-                                color: "#fff",
-                                fontSize: 15
-                            }
-                        }>Detail Screen ... Again 
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-
-                <View style={
-                    {
-                        paddingBottom: 10,
-                    }
-                }>
-                    <TouchableOpacity 
-                        onPress={
-                            () => navigation.navigate("Home")
-                        }
-                        style={
-                            {
-                                backgroundColor: "#FF2D55",
-                                paddingHorizontal: 30,
-                                paddingVertical: 7,
-                                textAlign: "center",
-                                borderRadius: 3,
-                            }
-                        }
-                        >
-                        <Text style={
-                            {
-                                color: "#fff",
-                                fontSize: 15
-                            }
-                        }>Go To Home
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-
-                <View style={
-                    {
-                        paddingBottom: 10,
-                    }
-                }>
-                    <TouchableOpacity 
-                        onPress={
-                            () => navigation.goBack()
-                        }
-                        style={
-                            {
-                                backgroundColor: "#FF2D55",
-                                paddingHorizontal: 30,
-                                paddingVertical: 7,
-                                textAlign: "center",
-                                borderRadius: 3,
-                            }
-                        }
-                        >
-                        <Text style={
-                            {
-                                color: "#fff",
-                                fontSize: 15
-                            }
-                        }>Go Back
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-
-                <View style={
-                    {
-                        paddingBottom: 10,
-                    }
-                }>
-                    <TouchableOpacity 
-                        onPress={
-                            () => navigation.popToTop()
-                        }
-                        style={
-                            {
-                                backgroundColor: "#FF2D55",
-                                paddingHorizontal: 30,
-                                paddingVertical: 7,
-                                textAlign: "center",
-                                borderRadius: 3,
-                            }
-                        }
-                        >
-                        <Text style={
-                            {
-                                color: "#fff",
-                                fontSize: 15
-                            }
-                        }>Go To The First Screen
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-        </SafeAreaView>
-    );
-};
 
 const Stack = createStackNavigator();
 
@@ -197,17 +20,17 @@ const App = () => {
             <Stack.Navigator screenOptions= {
                 {
                     headerStyle: {
-                        backgroundColor: "red"
+                        backgroundColor: "#D3DEAB"
                     },
                     headerTintColor: "#fff",
                     headerTitleStyle: {
                         fontWeight: "bold"
                     }
                 }
-            }>
+            } >
                 <Stack.Screen 
                     name="Home"
-                    component={HomeScreen} 
+                    component={Home_Screen} 
                     options= {
                         {
                             title: "Home Screen"
@@ -216,7 +39,7 @@ const App = () => {
                 />
                 <Stack.Screen 
                     name="Details"
-                    component={DetailScreen}
+                    component={Detail_Screen}
                     options= {
                         {
                             title: "Detail Screen"
@@ -229,3 +52,4 @@ const App = () => {
 }
 
 export default App;
+
